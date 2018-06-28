658a87ab8b83d26b87f1d0d8e6b6dcc04a6371de
Rich Jones <miserlou@gmail.com>
2018-06-28 15:19:46 -0400

    add base image

--- generate_matricies_from_scratch.sh
+++ generate_matricies_from_scratch.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+# Fetch the CELS
 virtualenv env
 source env/bin/activate
 pip install -r requirements.txt
 python acquire_cels.py
+
+# Build the base image
+docker build -t "convert/base" -f Dockerfile.base .
+
