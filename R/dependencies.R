562c33302ddfa6e770d7bb3196eb0655bc7d118f
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2018-11-07 16:20:54 -0500

    Pin devtools, use BiocInstaller::biocLite

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,23 +1,61 @@
-# Turn warnings into errors because biocLite throws warnings instead
-# of error if it fails to install something.
-options(warn=2)
-options(repos=structure(c(CRAN="https://cloud.r-project.org")))
-options(Ncpus=parallel::detectCores())
+# Pin devtools to version 1.13.6 & install its dependencies
+# devtools is the package we use to install specific versions of other packages
+# see also: https://github.com/AlexsLemonade/refinebio/pull/752
 
-# Install dev packages
-install.packages("devtools")
+# From https://github.com/AlexsLemonade/refinebio/blob/d55616dff8270aa46179a26c8e86318c8535df32/common/install_devtools.R
+
+# Treat warnings as errors, set CRAN mirror, and set parallelization:
+options(warn=2)
+options(repos=structure(c(CRAN="http://lib.stat.cmu.edu/R/CRAN/")))
+
+install_package_version <- function(package_name, version) {
+  # This function install a specific version of a package.
+
+  # However, because the most current version of a package lives in a
+  # different location than the older versions, we have to check where
+  # it can be found.
+  package_tarball <- paste0(package_name, "_", version, ".tar.gz")
+  package_url <- paste0("http://lib.stat.cmu.edu/R/CRAN/src/contrib/", package_tarball)
+
+  # Give CRAN a full minute to timeout since it's not always the most reliable.
+  curl_result <- system(paste0("curl --head --connect-timeout 60 ", package_url), intern=TRUE)
+  if (grepl("404", curl_result[1])) {
+    package_url <- paste0("http://lib.stat.cmu.edu/R/CRAN/src/contrib/Archive/", package_name, "/", package_tarball)
+
+    # Make sure the package actually exists in the archive!
+    curl_result <- system(paste0("curl --head --connect-timeout 120 ", package_url), intern=TRUE)
+    if (grepl("404", curl_result[1])) {
+      stop(paste("Package", package_name, "version", version, "does not exist!"))
+    }
+  }
+
+  install.packages(package_url)
+}
+
+install_package_version("jsonlite", "1.5")
+install_package_version("mime", "0.6")
+install_package_version("curl", "3.2")
+install_package_version("openssl", "1.0.2")
+install_package_version("R6", "2.3.0")
+install_package_version("httr", "1.3.1")
+install_package_version("digest", "0.6.18")
+install_package_version("memoise", "1.1.0")
+install_package_version("whisker", "0.3-2")
+install_package_version("rstudioapi", "0.8")
+install_package_version("git2r", "0.23.0")
+install_package_version("withr", "2.1.2")
+install_package_version("devtools", "1.13.6")
 
 # Use devtools::install_version() to install packages in cran.
 devtools::install_version('data.table', version='1.11.0')
 devtools::install_version('optparse', version='1.4.4')
+devtools::install_version('rlang', version='0.2.2')
+devtools::install_version('dplyr', version='0.7.4')
+devtools::install_version('readr', version='1.1.1')
+devtools::install_version('tidyr', version='0.8.2')
 
-# Bioconductor packages, installed by devtools::install_url()
-
-# devtools::install_url() requires biocLite.R
-source('https://bioconductor.org/biocLite.R')
-
-# Tidyverse and related
-install.packages("tidyverse")
+# BiocInstaller, required by devtools::install_url()
+install.packages('https://bioconductor.org/packages/3.6/bioc/src/contrib/BiocInstaller_1.28.0.tar.gz')
 
 # Helper function that installs a list of packages based on input URL
 install_with_url <- function(main_url, packages) {
--- R/install_pd.R
+++ R/install_pd.R
@@ -4,15 +4,13 @@ install_with_url <- function(main_url, packages) {
          function(pkg) devtools::install_url(paste0(main_url, pkg)))
 }
 
-source("https://bioconductor.org/biocLite.R")
-biocLite(paste0(Sys.getenv("DB"), '.db'))
-biocLite('pd.ht.hg.u133.plus.pm')
-biocLite('pd.ht.mg.430a')
-biocLite("htmg430aprobe")
+BiocInstaller::biocLite(paste0(Sys.getenv("DB"), '.db'))
+BiocInstaller::biocLite('pd.ht.hg.u133.plus.pm')
+BiocInstaller::biocLite('pd.ht.mg.430a')
+BiocInstaller::biocLite("htmg430aprobe")
 
 annotation_url <- 'https://bioconductor.org/packages/3.6/data/annotation/src/contrib/'
 pd_annotation_pkgs <- c(
 	Sys.getenv("PACKAGE")
 )
 install_with_url(annotation_url, pd_annotation_pkgs)
-
